8d1f70089472f08b64e2836e1d37ae6ea304bcc9
A-Yifeng <46978943+A-Yifeng@users.noreply.github.com>
2019-03-04 14:25:02 -0600

    Module 5A assignment completed

--- js/geojson_MyOwnData.js
+++ js/geojson_MyOwnData.js
@@ -29,40 +29,39 @@ function onEachFeature(feature, layer) {
     };
 };
 
+function calcR(map,attrValue) {
+	return Math.sqrt((attrValue * 0.5)/Math.PI) * 0.5
+}
+
+function createPropSymbols(data,map) {
+	var attr = "gdp_2005"
+
+	L.geoJson(data, {
+		pointToLayer: function(feature,latlng) {
+			var attrValue = Number(feature.properties[attr])
+			console.log(feature.properties, attrValue);
+			return L.circleMarker(latlng, 
+			{
+				fillColor: "#ff7800",
+				color: "#00aa00",
+				weight: 1,
+				opacity: 1,
+				fillOpacity: 0.8,
+				radius: calcR(map,attrValue)
+			})
+		}
+	}).addTo(map)
+}
+
 //function to retrieve the data and place it on the map
 function getData(map){
-    /* load the data using the ajax call method */
-    /* The data for this geojson file is scraped from an article from wikipedia at
-        https://en.wikipedia.org/wiki/List_of_Chinese_administrative_divisions_by_GDP_per_capita
-        The data comes from one of the tables in this article - the table on historical GDP(purchasing power parity) of 31 of 34 administrative
-        divisions in China. */
-    // The unit is in US dollar. 
-    // The position of each administrative division marker is the geometric center of each province/direct-controlled municipality
-    $.ajax("data/China_admin_gdp(ppp)_growth.geojson", {
+    $.ajax("data/China_gdp.geojson", {
         /* specify the file format the ajax method is to call*/
         dataType: "json",
-        /* upon a successful call, a function with the parameter of response is called to add the formatted data into the map layer */
         success: function(response){
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            }
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                /* onEachFeature method calls the function with the same name to add popup to each feature on the layer */
-                onEachFeature: onEachFeature,
-                /* an inline function is called to pass the new marker value (as a replacement of the default) to pointToLayer method */
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-                
-            }).addTo(map);
+        	createPropSymbols(response,map)
         }
-    });
-};
+    })
+}
 
 $(document).ready(createMap);
